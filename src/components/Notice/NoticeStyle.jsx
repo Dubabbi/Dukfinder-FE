a87693e2e971f73727b1238b21ee4d7bb54315b4
[Dubabbi] <[thdms3160@naver.com]>
2023-11-25 16:28:36 +0900

    Design: 공지 제목 디자인 추가

---
+++ src/components/Notice/NoticeStyle.jsx
@@ -0,0 +1,24 @@
+// NoticeStyle.jsx
+import styled from 'styled-components';
+
+export const NoticeWrapper = styled.div`
+    color: black;
+    width: 1200px;
+    margin: 0px auto;
+`
+
+export const Section = styled.section`
+  padding: 50px 0;
+`
+
+export const PageTitle = styled.div`
+  margin-bottom: 30px;
+`
+
+export const TitleText = styled.h3`
+  margin-top: 10px;  
+  margin-left: 10px;
+  font-size: 30px;
+  color: #333333;
+  font-weight: bold; 
+`
