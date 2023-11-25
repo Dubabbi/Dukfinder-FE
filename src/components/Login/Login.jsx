44d6d1005dcc56b74aa717463f7cba2ff1cc1f48
[Dubabbi] <[thdms3160@naver.com]>
2023-11-25 17:03:10 +0900

    Feat: 로그인 페이지 기본 구성

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,11 @@
+import * as M from '../Main/MainStyle';
+import * as L from './LoginStyle';
+
+export default function Login() {
+    return (
+        <M.MainWrapper>
+            
+        </M.MainWrapper>
+
+    );
+}
