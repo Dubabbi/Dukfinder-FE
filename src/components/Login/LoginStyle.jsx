810ccad717c6f5a244c34e302e41cfcf5400dfb7
[Dubabbi] <[thdms3160@naver.com]>
2023-11-26 16:14:57 +0900

    Design: 이메일 비밀번호 입력란 스타일 추가

--- src/components/Login/LoginStyle.jsx
+++ src/components/Login/LoginStyle.jsx
@@ -40,3 +40,77 @@ export const TitleWrap = styled.div`
     display: inline; /* 'Duk'를 한 줄에 표시하기 위해 추가 */
   }
 `;
+
+export const ContentWrap = styled.div`
+  margin-top: 26px;
+  flex: 1;
+`;
+
+export const LostPwMessage = styled.div`
+  position: fixed;
+  margin-top: 8px;
+  margin-left: 61%;
+  cursor: pointer;
+  width: 90%;
+  color: red;
+  font-size: 12px;
+`
+
+export const InputTitle = styled.div`
+  font-size: 15px;
+  margin-left: 5%;
+  width: 90%;
+  font-weight: 600;
+  color: #262626;
+`;
+
+export const InputWrap = styled.div`
+  display: flex;
+  border-radius: 8px;
+  padding: 16px;
+  margin-top: 8px;
+  margin-left: 5%;
+  width: 90%;
+  background-color: #EFF0F2;
+  border: 1px solid #e2e0e0;
+
+  &:focus-within {
+    border: 1px solid #9e30f4;
+  }
+`;
+
+export const Input = styled.input`
+  width: 100%;
+  outline: none;
+  border: none;
+  height: 17px;
+  font-size: 14px;
+  font-weight: 400;
+
+  &::placeholder {
+    color: #666666;
+  }
+`;
+
+export const ErrorMessageWrap = styled.div`
+  margin-top: 8px;
+  margin-left: 5%;
+  width: 90%;
+  color: #ef0000;
+  font-size: 12px;
+`;
+
+export const NoAccount = styled.div`
+  margin-bottom: 30px;
+  cursor: pointer;
+  text-align: center;
+  font-size: 13px;
+  p {
+    font-weight: bold;
+    display: inline; /* 'Duk'를 한 줄에 표시하기 위해 추가 */
+  }
+`
+
+export const UnderlinedText = styled.span`
+  text-decoration: underline;
+`;
