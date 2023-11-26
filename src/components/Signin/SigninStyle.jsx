b835f68862a12b397bad3dd8380a25de5f55031c
[Dubabbi] <[thdms3160@naver.com]>
2023-11-27 01:10:54 +0900

    Design: 커서 포인터 효과 비활성화

--- src/components/Signin/SigninStyle.jsx
+++ src/components/Signin/SigninStyle.jsx
@@ -17,15 +17,24 @@ export const BottomButton = styled.button`
   height: 10%;
   border: none;
   font-weight: bold;
+  font-size: 20px;
   border-radius: 10px;
   background-color: #9e30f4;
   color: white;
   cursor: pointer;
 
   &:disabled {
+    margin-left: 5%;
+    margin-top: 8%;
+    width: 90%;
+    padding: 16px;
+    height: 10%;
+    border: none;
+    font-weight: bold;
+    border-radius: 10px;
+    pointer-events: none; 
     background-color: #FFE665;
     font-size: 20px;
     color: #0d0d0d;
   }
 `;
-
