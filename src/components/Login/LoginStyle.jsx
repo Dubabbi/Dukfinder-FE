9e57c7f8173e7873026d78a31cdc1fac90eb74f0
[Dubabbi] <[thdms3160@naver.com]>
2023-11-26 15:39:37 +0900

    Design: 로그인 제목 디자인 추가

--- src/components/Login/LoginStyle.jsx
+++ src/components/Login/LoginStyle.jsx
@@ -27,3 +27,16 @@ export const Page = styled.div`
   background-color: #ffffff;
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
 `;
+
+export const TitleWrap = styled.div`
+  margin-top: 65px;
+  font-size: 26px;
+  font-weight: bold;
+  text-align: center;
+  color: #262626;
+  letter-spacing: 0.5px;
+  line-height: 1.5;
+  p {
+    display: inline; /* 'Duk'를 한 줄에 표시하기 위해 추가 */
+  }
+`;
