59166f4b27b7b0a3df0ee4149d573cfd3eb23cff
[Dubabbi] <[thdms3160@naver.com]>
2023-11-27 13:09:25 +0900

    Feat: 글 작성 페이지 기본 구성 추가

---
+++ src/components/Upload/Upload.jsx
@@ -0,0 +1,17 @@
+//Upload.jsx
+import * as U from './UploadStyle';
+import * as M from '../Main/MainStyle';
+import * as N from '../Notice/NoticeStyle';
+
+export default function Upload() {
+    return (
+        <M.MainWrapper>
+        <N.Section>
+          <N.PageTitle>
+            <N.TitleText>작성 페이지</N.TitleText>
+          </N.PageTitle>
+ 
+        </N.Section>
+        </M.MainWrapper>
+    );
+}
