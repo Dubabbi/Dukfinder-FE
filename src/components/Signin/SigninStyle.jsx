c1645d0dfb493c1f000b7b56060aeeb1248e7551
[Dubabbi] <[thdms3160@naver.com]>
2023-12-01 14:15:38 +0900

    Design: BottomButton 수정

--- src/components/Signin/SigninStyle.jsx
+++ src/components/Signin/SigninStyle.jsx
@@ -15,37 +15,52 @@ export const TitleWrap = styled.div`
 `;
 
 export const TextWrap = styled.div`
-  margin-bottom: 2%;
-  font-size: 15px;
+  padding: 10%;
+  font-size: 18px;
   text-align: center;
-  line-height: 1.5;
+  
+  line-height: 1.6;
+`;
+
+
+export const InputWrap = styled.div`
+  display: flex;
+  border-radius: 8px;
+  padding: 10px;
+  margin-top: 8px;
+  margin-left: 5%;
+  width: 90%;
+  background-color: #EFF0F2;
+  border: 1px solid #e2e0e0;
+
+  &:focus-within {
+    border: 1px solid #9e30f4;
+  }
+`;
+
+export const Input = styled.input`
+  width: 100%;
+  outline: none;
+  border: none;
+  height: 16px;
+  font-size: 15px;
+  font-weight: 400;
+
+  &::placeholder {
+    color: #666666;
+  }
 `;
 
 export const BottomButton = styled.button`
-  margin-left: 5%;
-  width: 90%;
-  margin-top: 5%;
-  height: 10%;
-  border: none;
-  font-weight: bold;
-  font-size: 20px;
-  border-radius: 10px;
-  background-color: #9e30f4;
-  color: white;
-  cursor: pointer;
-
-  &:disabled {
-    margin-left: 5%;
-    width: 90%;
-    margin-top: 5%;
-    padding: 16px;
-    height: 10%;
-    border: none;
-    font-weight: bold;
-    border-radius: 10px;
-    pointer-events: none; 
-    background-color: #FFE665;
-    font-size: 20px;
-    color: #0d0d0d;
-  }
+margin-left: 5%;
+margin-top: 8%;
+width: 90%;
+padding: 16px;
+height: 10%;
+border: none;
+font-weight: bold;
+background-color: #FFE665;
+font-size: 20px;
+color: #0d0d0d;
+border-radius: 10px;
 `;
