8d6c6da8f53e5c9cbfba0bc97cbcaec2e7c998d5
[Dubabbi] <[thdms3160@naver.com]>
2023-12-03 08:37:09 +0900

    Design: NoticeDetail 기본 디자인

---
+++ src/components/NoticeDetail/NoticeDetailStyle.jsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const DetailTitle = styled.h2`
+  font-size: 25px;
+  color: #333333; 
+
+  padding: 1%; 
+`;
+
+export const TitleWrap = styled.div`
+
+`;
+
+
+export const TextWrap = styled.div`
+  padding: 2%;
+  font-size: 18px;
+  line-height: 1.6;
+`;
+
