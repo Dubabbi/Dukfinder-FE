090bd5054347ac31e0c1688f656ebb084355c211
[Dubabbi] <[thdms3160@naver.com]>
2023-12-02 19:22:15 +0900

    Design: Upload margin 수정

--- src/components/Upload/UploadStyle.jsx
+++ src/components/Upload/UploadStyle.jsx
@@ -1,65 +1,54 @@
 // UploadStyle.jsx
 import styled from 'styled-components';
 
-export const CustomTableRow = styled.tr`
-  height: 30px;
-  width: 100%;
-  max-width: 100%;
-`;
-
-export const CustomTableCell = styled.td`
-  height: 30px; /* 원하는 높이를 지정하세요 */
-  max-width: 100%;
-  width: 100%;
-`;
-
+export const MainWrapper = styled.div`
+    color: black;
+    max-width: 1200px;
+    margin: 0px auto;
+`
 export const Wrapper = styled.div`
   width: 100%;
-  max-width: 100%;
-  margin: 0 auto;
   padding: 0 1%;
-  table{
-    max-width: 100%
-    width: 100%;
-  }
 `;
 
 
 export const Form = styled.form`
-  margin-top: 10%;
-  width: 100%
-  max-width: 100%
-  justify content: space-between;
-  align-items: center;
-  table {
-    max-width: 100%;
-    width: 100%;  
-  }
+  max-width: 100%;
+  margin-top: 7%;
 `;
+
+export const SecondForm = styled.form`
+  margin-top: 20px;
+  max-width: 100%;
+`;
+
 
 export const FormGroup = styled.div`
   margin-bottom: 14px;
   max-width: 100%;
-  justify content: space-between;
-  align-items: center;
-`;
-
-export const InlineFormGroup = styled.div`
-  margin-bottom: 14px;
-  max-width: 100%;
-  justify content: space-between;
-  // align-items: center;
-  display: inline-block;
 `;
 
 export const Inline = styled.div`
-  display: inline;
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  div{
+    display: inline-block;
+  }
+`;
+
+
+export const InlineImg = styled.div`
+  display: flex;
+  width: 100%;
+  div{
+    display: inline-block;
+  }
 `;
 
 export const Label = styled.label`
   font-size: 2rem;
-  margin-bottom: 6px; 
-  margin-top: 1%;
+  margin-top: 20px;
   display: block;
 `;
 
@@ -68,22 +57,31 @@ export const Input = styled.input`
   border: 1px #ddd solid;
   height: 40px;
   border-radius: 5px;
-  margin-bottom: 10px;
   font-size: 2rem;
   width: 100%;
   max-width: 100%;
   font-family: inherit;
 `;
 
+export const Select = styled.select`
+  background-color: #EEEEEE;
+  border: 1px #ddd solid;
+  height: 40px;
+  border-radius: 5px;
+  font-size: 2rem;
+  font-family: inherit;
+  width: 300px;
+`;
+
 export const Textarea = styled.textarea`
   background-color: #EEEEEE;
+  margin-top: 20px;
   border: 1px #ddd solid;
   border-radius: 5px;
   padding: 10px;
   font-size: 2rem;
   width: 100%;
-  max-width: 100%;
-  height: 100px;
+  height: 200px;
   font-family: inherit;
 `;
 
@@ -109,12 +107,13 @@ export const SubmitButton = styled.input.attrs({ type: 'submit', value: '저장'
 export const ImgButton = styled.button`
 border: none;
 cursor: pointer;
-line-height: 1;
-font-size: 1.375rem;
+font-size: 1.5rem;
 background: rgba(19, 19, 19, 0.334);
 color: #fff;
 border-radius: 10px;
 padding: 10px 20px;
+margin-left: 20px;
+margin-top: 15px;
 
 &:hover {
   background: #151515ae;
