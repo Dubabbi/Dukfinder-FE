b4c777c5e5ce495c44f55a332c02171472739bd2
[Dubabbi] <[thdms3160@naver.com]>
2023-12-03 07:20:28 +0900

    Feat: Login API

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -3,10 +3,6 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import * as L from './LoginStyle';
 
-const User = {
-  email: 'test@duksung.ac.kr',
-  pw: 'test2323@@@',
-};
 
 const Login = () => {
   const [email, setEmail] = useState('');
@@ -17,23 +13,25 @@ const Login = () => {
   const [notAllow, setNotAllow] = useState(true);
 
   useEffect(() => {
+    // Check both email and password conditions
     if (emailValid && pwValid) {
       setNotAllow(false);
-      return;
+    } else {
+      setNotAllow(true);
     }
-    setNotAllow(true);
   }, [emailValid, pwValid]);
-
+  
   const handleEmail = (e) => {
     setEmail(e.target.value);
     const regex =
-      /^(([^<>()\[\].,;:\s@"]+(\.[^<>()\[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
+    /^(?=.*[a-zA-Z]).*duksung\.ac\.kr$/;
     if (regex.test(e.target.value)) {
       setEmailValid(true);
     } else {
       setEmailValid(false);
     }
   };
+
   const handlePw = (e) => {
     setPw(e.target.value);
     const regex =
@@ -44,67 +42,81 @@ const Login = () => {
       setPwValid(false);
     }
   };
-  
-  const onClickConfirmButton = () => {
-    if (email === User.email && pw === User.pw) {
+
+  const onClickConfirmButton = async () => {
+    try {
+      // Axios를 사용하여 API 호출
+      const response = await axios.post('/user/login/', {
+        email: email, 
+        password: pw,
+      });
+
+      // 로그인 성공
       alert('로그인에 성공했습니다.');
-    } else {
+      console.log('토큰:', response.data.token);
+
+      // 토큰을 상태에 저장하거나 필요한 대로 사용
+    } catch (error) {
+      // 로그인 실패
       alert('등록되지 않은 회원입니다.');
+      console.error('에러:', error.response.data.error);
     }
   };
 
+  
   return (
     <L.LoginWrapper>
-    <L.Page>
-      <L.TitleWrap>
-        <p style={{ color: 'red'}}>🐤Duk</p><p>finder🐤에 오신 것을</p><br/>
-        환영합니다!👋
-      </L.TitleWrap>
-      {/* 🦆 */}
-      <L.ContentWrap>
-        <L.InputTitle>Email</L.InputTitle>
-        <L.InputWrap>
-          <L.Input
-            type="text"
-            placeholder="test@duksung.ac.kr"
-            value={email}
-            onChange={handleEmail}
-          />
+      <L.Page>
+        <L.TitleWrap>
+          <p style={{ color: 'red' }}>🐤Duk</p>
+          <p>finder🐤에 오신 것을</p>
+          <br />
+          환영합니다!👋
+        </L.TitleWrap>
+        {/* 🦆 */}
+        <L.ContentWrap>
+          <L.InputTitle>Email</L.InputTitle>
+          <L.InputWrap>
+            <L.Input
+              type="text"
+              placeholder="test@duksung.ac.kr"
+              value={email}
+              onChange={handleEmail}
+            />
+          </L.InputWrap>
+          <L.ErrorMessageWrap>
+            {!emailValid && email.length > 0 && (
+              <div>올바른 이메일을 입력해주세요.</div>
+            )}
+          </L.ErrorMessageWrap>
 
-        </L.InputWrap>
-        <L.ErrorMessageWrap>
-          {!emailValid && email.length > 0 && (
-            <div>올바른 이메일을 입력해주세요.</div>
-          )}
-        </L.ErrorMessageWrap>
+          <L.InputTitle style={{ marginTop: '26px' }}>Password</L.InputTitle>
+          <L.InputWrap>
+            <L.Input
+              type="password"
+              placeholder="Enter your password"
+              value={pw}
+              onChange={handlePw}
+            />
+          </L.InputWrap>
 
-
-        <L.InputTitle style={{ marginTop: '26px' }}>Password</L.InputTitle>
-        <L.InputWrap>
-          <L.Input
-            type="password"
-            placeholder="Enter your password"
-            value={pw}
-            onChange={handlePw}
-          />
-        </L.InputWrap>
-        {/* <L.LostPwMessage>
-        <L.UnderlinedText>
-          <a href = "#">비밀번호를 잊으셨나요?</a>
-        </L.UnderlinedText>
-        </L.LostPwMessage> */}
-      
-
-      <div>
-        <L.BottomButton onClick={onClickConfirmButton} disabled={notAllow}>
-          로그인
-        </L.BottomButton>
-        <L.NoAccount><p>계정이 없으신가요? </p><p style = {{color: 'blue'}}><L.UnderlinedText><a href = "./Signin"> 회원가입</a></L.UnderlinedText></p></L.NoAccount>
-      </div>
-      </L.ContentWrap>
-    </L.Page>
+          <div>
+            <L.BottomButton onClick={onClickConfirmButton} disabled={notAllow}>
+              로그인
+            </L.BottomButton>
+            <L.NoAccount>
+              <p>계정이 없으신가요? </p>
+              <p style={{ color: 'blue' }}>
+                <L.UnderlinedText>
+                  <a href="./Signin"> 회원가입</a>
+                </L.UnderlinedText>
+              </p>
+            </L.NoAccount>
+          </div>
+        </L.ContentWrap>
+      </L.Page>
     </L.LoginWrapper>
   );
 };
- 
+
 export default Login;
