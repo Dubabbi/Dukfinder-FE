5fce6b181fc44931642248348bf4e07654a2d4c0
techncherry <technarchitect@gmail.com>
2023-12-03 23:22:42 +0900

    Fix: 라우터 Nav 수정

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,24 +2,30 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MainPage from './pages/Main';
 import LostPage from './pages/Lost';
 import FindPage from './pages/Find';
+import FindDetailPage from './pages/FindDetail';
 import NoticePage from './pages/Notice';
+import NoticeDetailPage from './pages/NoticeDetail';
 import Mypage from './pages/Mypage';
 import LoginPage from './pages/Login';
 import SigninPage from './pages/Signin';
 import UploadPage from './pages/Upload';
 import Nav from './components/Navbar/Nav';
 import Footer from './components/Footer/Footer';
+import LostDetailPage from './pages/LostDetail';
 
 export default function Router() {
     return (
         <>
-            <Nav/>
             <BrowserRouter>
+            <Nav/>
                 <Routes>
                     <Route path="/" element={<MainPage />}/>
                     <Route path="/lost" element={<LostPage />}/>
+                    <Route path="/lost/:p_id" element={<LostDetailPage/>}/>
                     <Route path="/find" element={<FindPage />}/>
+                    <Route path="/find/:p_id" element={<FindDetailPage/>}/>
                     <Route path="/notice" element={<NoticePage />}/>
+                    <Route path="/notice/:n_id" element={<NoticeDetailPage />}/>
                     <Route path="/mypage" element={<Mypage />}/>
                     <Route path="/login" element={<LoginPage />}/>
                     <Route path="/signin" element={<SigninPage />}/>
