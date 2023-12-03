09bc14b64b65f4502c2a5b720389c4a9fe5dc2a7
[Dubabbi] <[thdms3160@naver.com]>
2023-12-03 23:55:08 +0900

    Feat: Login 기능 수정

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,12 @@
 // Login.jsx
+
+import { useNavigate } from 'react-router-dom';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import * as L from './LoginStyle';
 
 const Login = () => {
+  const baseURL = "https://port-0-dukfinder-57lz2alpp5sfxw.sel4.cloudtype.app/user/login";
   const [email, setEmail] = useState('');
   const [pw, setPw] = useState('');
 
@@ -14,47 +17,79 @@ const Login = () => {
   useEffect(() => {
     if (emailValid && pwValid) {
       setNotAllow(false);
-    } else {
-      setNotAllow(true);
+      return;
     }
+    setNotAllow(true);
   }, [emailValid, pwValid]);
 
   const handleEmail = (e) => {
     setEmail(e.target.value);
-    const regex = /^(?=.*[a-zA-Z]).*duksung\.ac\.kr$/;
-    setEmailValid(regex.test(e.target.value));
+    const regex =
+    /^[a-zA-Z0-9$`~!@$!%*#^?&\\(\\)\-_=+]{2,}$/;
+    if (regex.test(e.target.value)) {
+      setEmailValid(true);
+    } else {
+      setEmailValid(false);
+    }
   };
 
   const handlePw = (e) => {
     setPw(e.target.value);
-    const regex = /^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+])(?!.*[^a-zA-z0-9$`~!@$!%*#^?&\\(\\)\-_=+]).{8,20}$/;
-    setPwValid(regex.test(e.target.value));
+    const regex =
+     /^[a-zA-Z0-9$`~!@$!%*#^?&\\(\\)\-_=+]{2,}$/;
+    if (regex.test(e.target.value)) {
+      setPwValid(true);
+    } else {
+      setPwValid(false);
+    }
   };
 
+  const navigate = useNavigate();
+  
   const onClickConfirmButton = async () => {
     try {
       const response = await axios.post(
-        '/user/login/',
+        'https://port-0-dukfinder-57lz2alpp5sfxw.sel4.cloudtype.app/user/login/',
         {
           email: email,
           password: pw,
         },
-        { withCredentials: true } // Add this option to send cookies
+        {
+          withCredentials: true,  // Add this option to send cookies
+        }
       );
-
-      alert('로그인에 성공했습니다.');
-      console.log('토큰:', response.data.token);
-      history.push('/main');
-
+  
+      if (response.status === 200) {
+        // 로그인 성공 시
+        alert('로그인에 성공했습니다.');
+        console.log('토큰:', response.data.token);
+        // 토큰을 상태로 저장하거나 다른 곳에 활용할 수 있도록 처리
+        // 예: 저장된 토큰을 전역 상태로 관리하는 Context API 활용
+        // 여기서는 간단하게 alert으로 토큰을 표시
+        alert('토큰: ' + response.data.token);
+  
+        // navigate 함수를 사용하여 페이지 이동
+        navigate('/');
+      } else {
+        // 로그인 실패 시
+        alert('로그인에 실패했습니다.');
+      }
+  
     } catch (error) {
       if (error.response && error.response.status === 400) {
+        // 가입되지 않은 사용자의 경우
         alert('등록되지 않은 회원입니다.');
+      } else if (error.response && error.response.status === 401) {
+        // 인증 실패의 경우
+        alert('인증에 실패하였습니다. 이메일과 비밀번호를 확인하세요.');
       } else {
+        // 기타 오류의 경우
         alert('로그인에 실패했습니다.');
       }
       console.error('에러:', error.response ? error.response.data.error : error.message);
     }
   };
+  
   
   return (
     <L.LoginWrapper>
@@ -100,7 +135,7 @@ const Login = () => {
               <p>계정이 없으신가요? </p>
               <p style={{ color: 'blue' }}>
                 <L.UnderlinedText>
-                  <a href="./Signin"> 회원가입</a>
+                  <a href="/Signin"> 회원가입</a>
                 </L.UnderlinedText>
               </p>
             </L.NoAccount>
