75ebe7b18ae554eed0d7c0f0eb6915c8797ad2cf
[Dubabbi] <[thdms3160@naver.com]>
2023-12-03 23:57:46 +0900

    Feat: NoticeDetail

--- src/components/NoticeDetail/NoticeDetail.jsx
+++ src/components/NoticeDetail/NoticeDetail.jsx
@@ -1,38 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
-import { noticeData } from '../../noticeData';
+import axios from 'axios';
 import * as N from '../Notice/NoticeStyle';
 import * as D from './NoticeDetailStyle';
 
-function NoticeDetail(props) {
-    const { n_id } = useParams();
-    const notice = noticeData.results.find(notice => notice.n_id === n_id);
+function NoticeDetail() {
+  const { id } = useParams();
+  const [notice, setNotice] = useState(null);
 
-    // notice가 존재하지 않는 경우에 대한 예외 처리
-    if (!notice) {
-        return <div>해당 공지사항을 찾을 수 없습니다.</div>;
-    }
+  useEffect(() => {
+    // Axios를 사용하여 서버로부터 공지사항 상세 데이터를 가져옴
+    const fetchNoticeDetail = async () => {
+      try {
+        const response = await axios.get(`http://127.0.0.1:8000/notice/${id}`);
+        setNotice(response.data);
+      } catch (error) {
+        console.error('공지사항 상세 불러오기 실패:', error.message);
+      }
+    };
 
-    const { n_title, n_content } = notice;
+    fetchNoticeDetail();
+  }, [id]); // id가 변경될 때마다 useEffect가 실행되도록 설정
 
-    return (
-        <N.NoticeWrapper>
-            <N.Section>
-                <N.PageTitle>
-                    <N.TitleText>공지사항</N.TitleText>
-                </N.PageTitle>
-                <D.TitleWrap><hr />
-                <D.DetailTitle>
-                
-                    {n_title}
-                  
-                </D.DetailTitle> <hr />                  
-                </D.TitleWrap>
-                <D.TextWrap>{n_content}</D.TextWrap>
-                <button as={Link} to="../notice">목록으로</button>
-            </N.Section>
-        </N.NoticeWrapper>
-    );
+  // notice가 존재하지 않거나 로딩 중인 경우
+  if (!notice) {
+    return <div>로딩 중이거나 해당 공지사항을 찾을 수 없습니다.</div>;
+  }
+
+  const { title, content } = notice;
+
+  return (
+    <N.NoticeWrapper>
+      <N.Section>
+        <N.PageTitle>
+          <N.TitleText>공지사항</N.TitleText>
+        </N.PageTitle>
+        <D.TitleWrap>
+          <hr />
+          <D.DetailTitle>{title}</D.DetailTitle>
+          <hr />
+        </D.TitleWrap>
+        <D.TextWrap>{content}</D.TextWrap>
+        <button as={Link} to="../notice">
+          목록으로
+        </button>
+      </N.Section>
+    </N.NoticeWrapper>
+  );
 }
 
 export default NoticeDetail;
