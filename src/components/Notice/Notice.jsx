00c1209e10e23005607775532e7da84a9fccde8e
[Dubabbi] <[thdms3160@naver.com]>
2023-12-03 23:55:50 +0900

    Feat: Notice 수정

--- src/components/Notice/Notice.jsx
+++ src/components/Notice/Notice.jsx
@@ -1,17 +1,52 @@
-// Notice.jsx
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
-import { noticeData } from '../../noticeData';
-import * as N from './NoticeStyle';
 import { Link } from 'react-router-dom';
-
+import * as N from './NoticeStyle';
 
 const Notice = () => {
+  const [noticeList, setNoticeList] = useState([]);
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  // useEffect(() => {
+  //   const fetchNoticeList = async () => {
+  //     try {
+  //       const response = await axios.get('http://127.0.0.1:8000/notice');
+  //       setNoticeList(response.data);
+  //       console.log(response.data);
+  //     } catch (error) {
+  //       console.error('공지사항 리스트를 불러오지 못했습니다:', error.message);
+  //     }
+  //   };
+
+  //   fetchNoticeList();
+  // }, []);
+
+  useEffect(() => {
+    const token = localStorage.getItem('authToken');
+
+    if (token) {
+      axios.get('http://127.0.0.1:8000/notice', {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+        .then(response => {
+          setLoggedIn(true);
+        })
+        .catch(error => {
+          console.error('Error fetching data: ', error);
+        });
+    } else {
+      setLoggedIn(false);
+    }
+  }, []); // This useEffect runs once on component mount
+
   return (
     <N.NoticeWrapper>
-      <N.Section>
-        <N.PageTitle>
+      {loggedIn ? (
+        <N.Section>
+          <N.PageTitle>
           <N.TitleText>공지사항</N.TitleText>
         </N.PageTitle>
         {/* Board Search Area */}
@@ -19,8 +54,8 @@ const Notice = () => {
           <N.SearchWindow>
             <N.SearchWrap>
               <N.SearchInput type="search" placeholder="검색어를 입력해주세요." />
-              <svg xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 512 512" onClick={() => {/* Handle icon click */}}>
-                <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/>
+              <svg xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 512 512" onClick={() => { /* Handle icon click */ }}>
+                <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
               </svg>
             </N.SearchWrap>
           </N.SearchWindow>
@@ -36,23 +71,27 @@ const Notice = () => {
                 <N.ThViews>조회수</N.ThViews>
               </N.TableRow>
             </N.TableHead>
-            {/* Table Body content goes here */}
             <N.TableBody>
-              {noticeData.results.map((notice) => (
-                <N.TableRow key={notice.n_id}>
-                  <N.ThNum>{notice.n_id}</N.ThNum>
+              {noticeList.map((notice) => (
+                <N.TableRow key={notice.id}>
+                  <N.ThNum>{notice.id}</N.ThNum>
                   <N.ThTitle>
-                    <N.BoardLink as={Link} to={`./${notice.n_id}`}>{notice.n_title}</N.BoardLink>
+                    {/* Wrap the title in Link */}
+                    <N.BoardLink as={Link} to={`/notice/${notice.id}`}>{notice.title}</N.BoardLink>
                   </N.ThTitle>
-                  <N.ThDate>{notice.n_date}</N.ThDate>
-                  <N.ThViews>{notice.n_view}</N.ThViews>
+                  <N.ThDate>{notice.created_at}</N.ThDate>
+                  <N.ThViews>{notice.view_count}</N.ThViews>
                 </N.TableRow>
               ))}
             </N.TableBody>
           </N.BoardTable>
         </N.BoardListArea>
-      </N.Section>
+        </N.Section>
+      ) : (
+        <p>Please log in to view notices</p>
+      )}
     </N.NoticeWrapper>
+    
   );
 }
 
