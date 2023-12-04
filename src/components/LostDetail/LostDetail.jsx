7c18f73d577b3ebea1f19798706a6fc777245c38
techncherry <technarchitect@gmail.com>
2023-12-05 05:38:15 +0900

    Chore: git pull 충돌 해결

--- src/components/LostDetail/LostDetail.jsx
+++ src/components/LostDetail/LostDetail.jsx
@@ -4,13 +4,62 @@ import React, { useEffect, useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import LostDetailCard from './LostDetailCard';
 import Image from 'react-bootstrap/Image';
+import axios from 'axios';
 import CommentMain from "../Comment/CommentMain";
 
 
 function LostDetail(props) {
     const { p_id } = useParams();
-    const post = data.results.find(post => post.p_id === p_id);
-   
+    const [post, setPost] = useState(null);
+    const [loggedIn, setLoggedIn] = useState(false); // 로그인 여부 상태
+
+    useEffect(() => {
+        const token = localStorage.getItem('key');
+        if (token) {
+            loadPost(token);
+        } else {
+            setLoggedIn(false);
+        }
+    }, []);
+
+    const loadPost = (token) => {
+        axios.get(`https://port-0-dukfinder-57lz2alpp5sfxw.sel4.cloudtype.app/user/userinfo/`, {
+            headers: {
+                Authorization: `Token ${token}`
+            }
+        })
+        .then(response => {
+            setLoggedIn(true);
+            fetchPost(token);
+        })
+        .catch(error => {
+            setLoggedIn(false);
+            console.error('Invalid token:', error);
+        });
+    };
+
+    const fetchPost = (token) => {
+        axios.get(`https://port-0-dukfinder-57lz2alpp5sfxw.sel4.cloudtype.app/lost_posts/${p_id}`, {
+            headers: {
+                Authorization: `Token ${token}`
+            }
+        })
+        .then(response => {
+            setPost(response.data);
+            console.log('포스트를 불러왔습니다.');
+        })
+        .catch(error => {
+            console.error('Error fetching data: ', error);
+        });
+    };
+
+    if (!post) {
+        return <div>Loading...</div>; // 데이터가 로딩 중일 때 표시할 내용
+    }
+
+    if (!loggedIn) {
+        return <Redirect to="../login" />; // 로그인되지 않았다면 로그인 페이지로 리다이렉트
+    }
     return (
         <>  
             <D.FindDetailWrapper smallSize>
@@ -25,13 +74,13 @@ function LostDetail(props) {
                 </D.PathContainer>
 
                 <D.DetailContainer>
-                    <D.ImageSize src={post.p_img} fluid smallSize/> 
+                    <D.ImageSize src={post.head_image} fluid smallSize/> 
                     <LostDetailCard post={post}/>
                 </D.DetailContainer>
                 <D.line></D.line>
-                <CommentMain postId={p_id}/>
+                {/* <CommentMain postId={p_id}/> */}
                 <D.ButtonContainer>
-                    <D.ButtonStyle variant="warning" type="submit" as={Link} to={`../find`}>목록으로</D.ButtonStyle>
+                    <D.ButtonStyle variant="warning" type="submit" as={Link} to={`../lost`}>목록으로</D.ButtonStyle>
                 </D.ButtonContainer>
             </D.FindDetailWrapper>
         </>
--- src/components/LostDetail/LostDetailCard.jsx
+++ src/components/LostDetail/LostDetailCard.jsx
@@ -8,35 +8,36 @@ import { Link } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
-function LostDetailCard({post}) {
-    // const { p_id } = useParams();
-    // const post = data.results.find(post => post.p_id === p_id);
-   
-    const navigate = useNavigate();
+function LostDetailCard(props) {
+    const { p_id } = useParams();
+    const { post } = props;
+
+
+    // const navigate = useNavigate();
 
     const [show, setShow] = useState(false);
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
-    const handleDelete = () => {
-        deletePost(post.p_id);
+    // const handleDelete = () => {
+    //     deletePost(post.p_id);
 
-        console.log('포스트를 삭제합니다:', post.p_id);
-        handleClose();
-        navigate('/lost');// 목록 페이지로 이동
-    };
+    //     console.log('포스트를 삭제합니다:', post.p_id);
+    //     handleClose();
+    //     navigate('/lost');// 목록 페이지로 이동
+    // };
 
-    const deletePost = (postId) => {
-        const updatedData = data.results.filter(item => item.p_id !== postId);
-        data.results = updatedData; // 가상의 data 객체 업데이트 (실제로는 데이터베이스 등에 반영해야 함)
-    };
+    // const deletePost = (postId) => {
+    //     const updatedData = data.results.filter(item => item.p_id !== postId);
+    //     data.results = updatedData; // 가상의 data 객체 업데이트 (실제로는 데이터베이스 등에 반영해야 함)
+    // };
 
     return (
         <D.CardStyle style={{ width: '65rem' }}>
             <D.YellowCardBody>
                 <D.DeleteContainer>
-                    <D.CardTitle>{post.p_title}</D.CardTitle>
+                    <D.CardTitle>{post.title}</D.CardTitle>
                     <D.DeleteButton variant="warning" type="submit" onClick={handleShow}>삭제</D.DeleteButton>
                     <Modal show={show} onHide={handleClose}>
                         <Modal.Header closeButton>
@@ -46,25 +47,25 @@ function LostDetailCard({post}) {
                             <D.ModalCloseButton variant="secondary" onClick={handleClose}>
                                 취소
                             </D.ModalCloseButton>
-                            <D.ModalButton variant="warning" onClick={handleDelete}>
+                            <D.ModalButton variant="warning" onClick={handleClose}>
                                 삭제하기
                             </D.ModalButton>
                         </Modal.Footer>
                     </Modal>
                 </D.DeleteContainer>
-                <D.SubtitleContainer className="mb-3 text-muted"><span>분실날짜:</span> {post.p_date}</D.SubtitleContainer>
-                <D.SubtitleContainer className="mb-3 text-muted"><span>분실장소:</span> {post.p_findPlace}</D.SubtitleContainer>
-                <D.SubtitleContainer className="mb-3 text-muted"><span>상태:</span> {post.p_status}</D.SubtitleContainer>
-                <D.SubtitleContainer className="mb-3 text-muted"><span>분류:</span> {post.p_category}</D.SubtitleContainer>
+                <D.SubtitleContainer className="mb-3 text-muted"><span>분실날짜:</span> {post.date_select}</D.SubtitleContainer>
+                <D.SubtitleContainer className="mb-3 text-muted"><span>분실장소:</span> {post.location}</D.SubtitleContainer>
+                <D.SubtitleContainer className="mb-3 text-muted"><span>상태:</span> {post.found_status}</D.SubtitleContainer>
+                <D.SubtitleContainer className="mb-3 text-muted"><span>분류:</span> {post.category}</D.SubtitleContainer>
                 <D.CardContent>
-                    {post.p_content.split('\n').map((content, index) => (
+                    {post.content.split('\n').map((content, index) => (
                         <React.Fragment key={index}>
                             {content}
-                            {index !== post.p_content.split('\n').length - 1 && <br />} {/* 마지막 줄에는 <br /> 추가하지 않음 */}
+                            {index !== post.content.split('\n').length - 1 && <br />} {/* 마지막 줄에는 <br /> 추가하지 않음 */}
                         </React.Fragment>
                     ))}
                 </D.CardContent>
-                <D.TimeContainer className="mb-3 text-muted">{new Date(post.p_time).toLocaleDateString()}</D.TimeContainer>
+                <D.TimeContainer className="mb-3 text-muted">{new Date(post.created_at).toLocaleDateString()}</D.TimeContainer>
             </D.YellowCardBody>
         </D.CardStyle>
 
