ed92305fb871f391f5f7edc09a22ee8bcd36eefe
techncherry <technarchitect@gmail.com>
2023-12-05 05:39:32 +0900

    Chore: git pull 충돌 해결

--- src/components/Lost/Lost.jsx
+++ src/components/Lost/Lost.jsx
@@ -1,19 +1,62 @@
 import * as L from './LostStyle';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import LostPostCard from './LostPostCard';
 import { data } from '../../postData';
 import SearchBar from '../SearchBar/SearchBar';
 import { Link } from 'react-router-dom';
 import DateFilter from '../DateFilter/DateFilter';
 import CategoryFilter from '../CategoryFilter/CategoryFilter';
-
+import axios from 'axios';
 
 function Lost() {
     const [searchTerm, setSearchTerm] = useState('');
     const [selectedCategory, setSelectedCategory] = useState('');
 
+    const [loggedIn, setLoggedIn] = useState(false); // 로그인 여부 상태
+    const [lostPostData, setLostPostData] = useState([]);
+
+    useEffect(() => {
+        const token = localStorage.getItem('key');
+        console.log('토큰 값:', token); // 토큰 값 확인
+
+        if (token) {
+            axios.get('https://port-0-dukfinder-57lz2alpp5sfxw.sel4.cloudtype.app/user/userinfo/', {
+                headers: {
+                    Authorization: `Token ${token}`
+                }
+            })
+                .then(response => {
+                    setLoggedIn(true);
+
+                    // 로그인 상태일 때 포스트를 불러옴
+                    axios.get('https://port-0-dukfinder-57lz2alpp5sfxw.sel4.cloudtype.app/lost_posts/', {
+                        headers: {
+                            Authorization: `Token ${token}` // 토큰을 헤더에 추가
+                        }
+                    })
+                        .then(response => {
+                            setLostPostData(response.data);
+                            console.log('포스트를 불러왔습니다.');
+                        })
+                        .catch(error => {
+                            console.error('Error fetching data: ', error);
+                        });
+                })
+                .catch(error => {
+                    setLoggedIn(false);
+                    console.error('Invalid token:', error);
+                });
+        } else {
+            setLoggedIn(false); // 토큰이 없다면, 로그인되지 않은 상태로 설정
+        }
+    }, []);
+
     const handleCategoryFilter = (category) => {
         setSelectedCategory(category.toLowerCase()); // 선택된 카테고리 업데이트
+    };
+
+    const handleDataUpdate = (data) => {
+        setLostPostData(data); // 받아온 데이터로 상태 업데이트
     };
 
     const handleSearch = (term) => {
@@ -21,9 +64,9 @@ function Lost() {
     };
 
     // 검색어와 선택된 카테고리에 따라 데이터 필터링하는 함수
-    const filteredPosts = data.results.filter(post => {
-        const matchesCategory = !selectedCategory || post.p_category.toLowerCase() === selectedCategory;
-        const matchesSearch = !searchTerm || post.p_title.toLowerCase().includes(searchTerm);
+    const filteredPosts = lostPostData.filter(post => {
+        const matchesCategory = !selectedCategory || post.category.toLowerCase() === selectedCategory;
+        const matchesSearch = !searchTerm || post.title.toLowerCase().includes(searchTerm);
 
         // 검색어와 카테고리 모두 일치하는 경우에만 반환
         return matchesCategory && matchesSearch;
@@ -33,33 +76,40 @@ function Lost() {
     return (
         <>
             <L.LostWrapper>
-                <SearchBar setSearchTerm={setSearchTerm} />
-                <CategoryFilter handleCategoryFilter={handleCategoryFilter} />
+                <SearchBar setSearchTerm={handleSearch} />
+                <CategoryFilter handleCategoryFilter={handleCategoryFilter} handleDataUpdate={handleDataUpdate} />
                 <L.Title>잃어버렸어요!
                     <L.NoticeTitle>
                         <span>UPDATE</span>
-                        <Link to="/your-link-here">공지 - 게시글 작성 방법 안내</Link>
+                        <Link to="/notice">공지 - 게시글 작성 방법 안내</Link>
                     </L.NoticeTitle>
                     <DateFilter />
                 </L.Title>
 
 
                 <L.Post_Container>
-                    {
-                        filteredPosts.map((item) => (
+                    {loggedIn ? (
+                        filteredPosts.map((post) => (
 
                             <LostPostCard
-                                p_id={item.p_id}
-                                p_img={item.p_img}
-                                p_title={item.p_title}
-                                p_date={item.p_date}
-                                p_findPlace={item.p_findPlace}
-                                p_category={item.p_category}
-                                p_status={item.p_status}
-                                p_content={item.p_content}
-                                p_time={item.p_time}
+                                p_img={post.head_image}
+                                p_id={post.id}
+                                p_title={post.title}
+                                p_date={post.date_select}
+                                p_status={post.found_status}
+                                p_lostPlace={post.location}
+                                p_category={post.category}
+                                p_content={post.content}
+                                p_createtime={post.created_at}
+                                p_updatetime={post.updated_at}
+                                p_author={post.author}
+                                p_comment={post.comments}
                             />
-                        ))}
+                        ))
+                    ) : (
+                        <h1>Please log in to view posts</h1>
+                    )}
+
 
 
                 </L.Post_Container>
