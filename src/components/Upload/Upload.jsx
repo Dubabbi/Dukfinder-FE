086e27f6426657fc772a44313fb7f208d496a02c
[Dubabbi] <[thdms3160@naver.com]>
2023-12-05 14:32:29 +0900

    Feat: Upload 수정

--- src/components/Upload/Upload.jsx
+++ src/components/Upload/Upload.jsx
@@ -1,27 +1,48 @@
+// Upload.jsx
+
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import DatePicker from 'react-datepicker';
+import './Upload.css'
 import 'react-datepicker/dist/react-datepicker.css';
-import * as N from '../Notice/NoticeStyle';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {FaCalendarAlt} from 'react-icons/fa';
 import * as U from './UploadStyle'; 
+import * as N from '../Notice/NoticeStyle'; 
 import styled from 'styled-components';
+import { setDate } from 'date-fns';
 
-const Upload = () => {
+const CustomInput = ({ value, onClick }) => (
+  <div className='input-group'>
+    <input type='text' className='form-control' value={value} onClick={onClick} readOnly />
+    <U.Inline><div className='input-group-append'></div>
+      <div><span className='input-group-text'>
+        <FaCalendarAlt />
+      </span></div></U.Inline>
+    
+  </div>
+);
+
+
+export default function Upload() {
+  const [loggedIn, setLoggedIn] = useState(false);
   const [selectedLocation, setSelectedLocation] = useState('');
   const [selectedCategory, setSelectedCategory] = useState('');
-  const [selectedDate, setSelectedDate] = useState(null);
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
-  const [locations, setLocations] = useState([]);
-  const [categories, setCategories] = useState([]);
+  const [location, setLocation] = useState([]);
+  const [category, setCategory] = useState([]);
   const navigate = useNavigate();
 
-  // 이벤트 핸들러들
   const handleTitleChange = (e) => setTitle(e.target.value);
   const handleLocationChange = (e) => setSelectedLocation(e.target.value);
   const handleCategoryChange = (e) => setSelectedCategory(e.target.value);
   const handleContentChange = (e) => setContent(e.target.value);
+
+  const [selectedDate, setSelectedDate] = useState(null);
+
+
 
   // 등록 버튼 클릭 시 서버로 데이터 전송
   const handleSubmit = async () => {
@@ -57,27 +78,55 @@ const Upload = () => {
     }
   };
 
-  // 페이지 로딩 시 필요한 데이터 초기화
   useEffect(() => {
     const token = localStorage.getItem('key');
+    console.log('토큰 값:', token);
 
     if (token) {
-      // 장소와 분류 카테고리 목록 가져오기
-      axios.get('https://port-0-dukfinder-57lz2alpp5sfxw.sel4.cloudtype.app/find_posts/category/location', {
-        headers: { Authorization: `Token ${token}` }
+      axios.get('https://port-0-dukfinder-57lz2alpp5sfxw.sel4.cloudtype.app/user/userinfo/', {
+        headers: {
+          Authorization: `Token ${token}`
+        }
       })
-        .then(response => setLocations(response.data))
-        .catch(error => console.error('Error fetching locations:', error));
+      .then(response => {
+        setLoggedIn(true);
 
-      axios.get('https://port-0-dukfinder-57lz2alpp5sfxw.sel4.cloudtype.app/find_posts/category/<str:category>', {
-        headers: { Authorization: `Token ${token}` }
+        axios.get('https://port-0-dukfinder-57lz2alpp5sfxw.sel4.cloudtype.app/find_posts/category/{category}', {
+          headers: {
+            Authorization: `Token ${token}`
+          }
+        })
+        .then(response => {
+          setFindPostData(response.data);
+          console.log('카테고리를 불러왔습니다.');
+        })
+        .catch(error => {
+          console.error('Error fetching data: ', error);
+        });
       })
-        .then(response => setCategories(response.data))
-        .catch(error => console.error('Error fetching categories:', error));
+      .catch(error => {
+        setLoggedIn(false);
+        console.error('Invalid token:', error);
+        navigate.push('/'); // Redirect to the login page if token is invalid
+      });
     } else {
-      navigate('/');
+      setLoggedIn(false);
+      // navigate.push('/'); // Redirect to the login page if token is not present
     }
   }, [navigate]);
+  const [findPostData, setFindPostData] = useState([]);
+
+  const locations = findPostData.map((location) => (
+      <option key={location.id}>
+      {location.name}
+    </option>
+  ));
+
+  const categories = findPostData.map((category) => (
+    <option key={category.id}>
+    {category.name}
+  </option>
+));
 
   return (
     <U.MainWrapper>
@@ -91,7 +140,11 @@ const Upload = () => {
             <div>
               <U.FormGroup>
                 <U.Label style={{ top: '10%' }}>제목</U.Label>
-                <U.Input value={title} onChange={handleTitleChange} />
+                <U.Input 
+                type="text"
+                placeholder="습득한 물건명"
+                value={title} 
+                onChange={handleTitleChange} />
               </U.FormGroup>
             </div>
             <U.Inline>
@@ -99,36 +152,31 @@ const Upload = () => {
                 <U.Label>장소</U.Label>
                 <U.Select value={selectedLocation} onChange={handleLocationChange}>
                   <option value="" disabled hidden>장소를 선택하세요</option>
-                  {locations.map(location => (
-                    <option key={location.id} value={location.name}>
-                      {location.name}
+                    <option>
+                      {locations}
                     </option>
-                  ))}
                 </U.Select>
               </div>
               <div>
                 <U.Label>분류</U.Label>
                 <U.Select value={selectedCategory} onChange={handleCategoryChange}>
-                  <option value="" disabled hidden>분류를 선택하세요</option>
-                  {categories.map(category => (
-                    <option key={category.id} value={category.name}>
-                      {category.name}
+                  <option value="" disabled hidden>습득물 분류</option>
+                    <option>
+                      {locations}
                     </option>
-                  ))}
                 </U.Select>
               </div>
               <div>
                 <U.Label>일자</U.Label>
+                {/* customInput 컴포넌트 사용 */}
                 <U.DatePickerWrapper>
-                  <DatePicker
-                    selected={selectedDate}
-                    onChange={(date) => setSelectedDate(date)}
-                    dateFormat="yyyy-MM-dd"
-                    showYearDropdown
-                    scrollableYearDropdown
-                    yearDropdownItemNumber={15}
-                    styles={{ width: '100px;' }}
-                  />
+                <div className='App'>
+      <DatePicker selected={selectedDate} onChange={date => setDate(date)}/>
+    </div>
+                <DatePicker
+                  selected={selectedDate}
+                  onChange={(date) => setSelectedDate(date)}
+                  customInput={<CustomInput />}/>
                 </U.DatePickerWrapper>
               </div>
             </U.Inline>
@@ -157,4 +205,3 @@ const Upload = () => {
   );
 };
 
-export default Upload;
