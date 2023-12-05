4f7cb91267b88205eb89767880f7eda0f1a41217
techncherry <technarchitect@gmail.com>
2023-12-06 00:53:49 +0900

    Feat: 로그아웃, 회원탈퇴 구현

--- src/components/Mypage/Setting.jsx
+++ src/components/Mypage/Setting.jsx
@@ -1,104 +1,71 @@
-import * as L from './../Login/LoginStyle';
-import * as S from './../Signin/SigninStyle';
-import React, { useEffect, useState } from 'react';
+import * as L from '../Login/LoginStyle';
+import * as Set from './SettingStyle';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 export default function Setting() {
-    const [oldPassword, setOldPassword] = useState('');
-    const [newPassword, setNewPassword] = useState('');
-    const [confirmNewPassword, setConfirmNewPassword] = useState('');
-    const [passwordMatchError, setPasswordMatchError] = useState('');
-    const [changeSuccess, setChangeSuccess] = useState(false);
-    const [data, setData] = useState([]);
-    const [loggedIn, setLoggedIn] = useState(false); // 로그인 여부 상태
-  
-    useEffect(() => {
-      const token = localStorage.getItem('key');
-  
-      if (token) {
-        // 유저 정보를 받아오는 요청
-        axios.get('https://port-0-dukfinder-57lz2alpp5sfxw.sel4.cloudtype.app/user/userinfo/', {
-          headers: {
-            Authorization: `Token ${token}`
-          }
-        })
-        .then(response => {
-            setData(response.data);})
-        .catch(error => {
-          console.error('Error fetching user info: ', error);
-          setLoggedIn(false); // 로그인되지 않은 상태로 설정
-        });
-      } else {
-        setLoggedIn(false); // 토큰이 없으면 로그인되지 않은 상태로 설정
-      }
-    }, []);
 
-  const handlePasswordChange = async () => {
-    try {
+   
+    const navigate = useNavigate();
+    
+    const handleLogout = () => {
+        // 로그아웃 확인 창 띄우기
+        const confirmLogout = window.confirm('로그아웃 하시겠습니까?');
+    
+        if (confirmLogout) {
+        // 로컬 스토리지에서 토큰 제거
+        localStorage.removeItem('key');
+        window.confirm('로그아웃 되었습니다');
+    
+        // "/" 페이지로 이동
+        navigate('/');
+        }
+    };
+
+    const handleWithdrawal = () => {
+        const confirmWithdrawal = window.confirm('정말 탈퇴하시겠습니까?');
+    
+        if (confirmWithdrawal) {
         const token = localStorage.getItem('key');
-
-      const response = await axios.put(
-        'https://port-0-dukfinder-57lz2alpp5sfxw.sel4.cloudtype.app/user/password/',
-        {
-          old_password: oldPassword,
-          new_password: confirmNewPassword,
-        },
-        {
+    
+        if (token) {
+            axios.delete('https://port-0-dukfinder-57lz2alpp5sfxw.sel4.cloudtype.app/user/userinfo/', {
             headers: {
-              Authorization: `Token ${token}`
-            },
-          }
-      );
+                Authorization: `Token ${token}` // 토큰을 헤더에 추가
+            }
+            })
+            .then(response => {
+                alert('탈퇴되었습니다.');
+                localStorage.removeItem('key');
+            })
+            .catch(error => {
+                console.error('Error withdrawing: ', error);
+                alert('탈퇴에 실패했습니다.');
+            });
+        } else {
+            alert('토큰이 없습니다. 로그인 상태를 확인해주세요.');
+        }
+        }
+    };
 
-      if (response.status === 200) {
-        setChangeSuccess(true);
-        // 비밀번호 변경이 성공한 경우에 수행할 작업
-        // 예를 들어, 성공 메시지를 보여줄 수 있습니다.
-      } else {
-        // 다른 응답에 대한 처리
-      }
-    } catch (error) {
-      console.error('비밀번호 변경 중 에러 발생:', error);
-      // 에러 발생 시 처리
-    }
-  };
-
-    return (
+    return(
         <L.LoginWrapper>
-        <L.Page>
+        <Set.Page>
           <L.TitleWrap>
-            <p>회원정보 수정</p>
+            <p>Setting</p>
           </L.TitleWrap>
           <L.ContentWrap>
-            <L.InputTitle>Current Password</L.InputTitle>
-            <S.InputWrap>
-              <S.Input
-                type="password"
-                placeholder="Enter your password"
-                value={oldPassword}
-                onChange={(e) => setOldPassword(e.target.value)}
-              />
-              {/* {!passwordValid && password.length > 0 && (
-                <L.ErrorMessageWrap>영문, 숫자, 특수기호 조합 8자리 이상의 비밀번호를 입력하세요.</L.ErrorMessageWrap>
-              )} */}
-            </S.InputWrap>
+            <Set.LinkWrap onClick={handleLogout}>로그아웃</Set.LinkWrap>
           </L.ContentWrap>
           <L.ContentWrap>
-            <L.InputTitle>New Password</L.InputTitle>
-            <S.InputWrap>
-              <S.Input
-                type="password"
-                placeholder="Re-enter your password"
-                value={confirmNewPassword}
-                onChange={(e) => setConfirmNewPassword(e.target.value)}
-              />
-              {passwordMatchError && <L.ErrorMessageWrap>{passwordMatchError}</L.ErrorMessageWrap>}
-            </S.InputWrap>
+            <a href="/mypage/setting/pw"><Set.LinkWrap>비밀번호 변경</Set.LinkWrap></a>
           </L.ContentWrap>
-          <L.BottomButton onClick={handlePasswordChange} >
-            {changeSuccess ? '변경 완료' : '비밀번호 변경'}
-          </L.BottomButton>
-        </L.Page>
+          <L.ContentWrap>
+            <Set.LinkWrap onClick={handleWithdrawal}>회원탈퇴</Set.LinkWrap>
+          </L.ContentWrap>
+        </Set.Page>
       </L.LoginWrapper>
-      );
+  );
+
 }
