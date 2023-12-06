167fff15518e73d2230a576cfeae4e3d62e4da71
sylvia1213 <yeayoung1213@naver.com>
2023-12-06 10:26:40 +0900

    Feat: lostUpload 수정 완료

--- src/pages/LostUpload.jsx
+++ src/pages/LostUpload.jsx
@@ -1,4 +1,4 @@
-import PostCreationPage from '../components/Upload/FindUpload';
+import PostCreationPage from '../components/Upload/LostUpload';
 
 export default function LostUploadPage() {
     return <><PostCreationPage/></>;
