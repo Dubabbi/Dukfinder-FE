97252deeaeb87436b0c154a85a0c0f198ff65eb4
techncherry <technarchitect@gmail.com>
2023-12-06 15:29:11 +0900

    Design: Nav에 로고,서비스명 추가

--- src/components/Navbar/NavStyle.jsx
+++ src/components/Navbar/NavStyle.jsx
@@ -3,22 +3,46 @@ import styled from 'styled-components';
 export const NavWrapper = styled.div`
     background-color: #FFE665;
     width : 100%;
-    height : 7vh;
+    height : 5vw;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`
+
+export const LinkWrapper = styled.div`
     display: flex;
     justify-content: flex-end;
     align-items: center;
 `
 
+export const Header = styled.div`
+    display: flex;
+    align-items: center;
+`
+
+export const Duck = styled.img`
+    width: 43px;
+    height: 43px;
+    margin: 4px 0 3px 20px;
+`
+
 export const Page = styled.div`
     color: #5F6368;
-    font-size: 20px;
+    font-size: 25px;
     vertical-align: center;
     padding: 10px;
 `
 
+export const Title = styled.div`
+    font-size: 30px;
+    display: flex;
+    align-items: center;
+    margin-left: 10px;
+`
+
 export const Logo = styled.img`
-    width : 25px;
-    height : 25px;
+    width : 40px;
+    height : 40px;
     margin : 0 10px 0 10px;
 `
 
